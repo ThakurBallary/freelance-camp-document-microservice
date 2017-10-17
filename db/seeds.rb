f24e4749d30ef3c82734c46041cb986e5f82ed30
Thakur <thakurballary@gmail.com>
2017-10-17 14:12:55 +0530

    Initial Commit

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+10.times do |d|
+	FreelanceDocument.create!(
+		title: "Document #{d}",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+		file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
+		image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
+	)
+end
